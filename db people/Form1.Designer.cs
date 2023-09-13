96988440fe8dc3fce523f19d17ef61cb9b43bc5c
mxxshlkxstch <knstntn0090@gmail.com>
2023-09-13 12:12:56 +0400

    ver 7 people

--- db people/Form1.Designer.cs
+++ db people/Form1.Designer.cs
@@ -78,7 +78,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(917, 450);
+            this.ClientSize = new System.Drawing.Size(865, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView2);
