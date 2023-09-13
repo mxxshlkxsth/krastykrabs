bf36209c0ba8b83db0820683ecd7f86980b5f68a
mxxshlkxstch <knstntn0090@gmail.com>
2023-09-13 11:10:06 +0400

    ver1 people

---
+++ db people/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data;
+
+namespace db_people
+{
+    public partial class Form1 : Form
+    {
+        public SQLiteConnection DB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            DB = new SQLiteConnection(Database.connectionString);
+            await DB.OpenAsync();
+        }
+
+        static class Database //строка подключения БД
+        {
+            public static string connectionString = @"Data Source=people.db;Integrated Security=False; MultipleActiveResultSets=True";
+        }
+
+        static class People_table
+        {
+            public static string main = "People";
+            public static string ID = "ID";
+            public static string Name = "Name";
+            public static string Foto = "Foto";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("SELECT * FROM People", DB);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            dataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            dataAdapter.Fill(ds, "Info");
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
---
+++ db people/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace db_people
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static class Database //строка подключения БД
+        {
+            public static string connectionString = @"Data Source=people.db;Integrated Security=False; MultipleActiveResultSets=True";
+        }
+        static class People_table
+        {
+            public static string main = "People";
+            public static string ID = "ID";
+            public static string Name = "Name";
+            public static string Foto = "Foto";
+        }
+
+
+    }
+}
